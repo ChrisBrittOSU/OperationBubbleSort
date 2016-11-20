daf0bc0a3fa0d9744bcc5350aa894aaa9e178bd4
Chris Britt <britt.88@osu.edu>
2016-11-19 21:55:53 -0500

    Renenabled the Start

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -74,11 +74,11 @@ public class Player : MonoBehaviour {
 		m_rigidBody = GetComponent<Rigidbody2D>();
         grounder.isGrounded = true;
 
-		lib.Map myMap = new lib.Map();
-		myMap.generate(100,100, 10);
-		myMap.printFile(@"Grid.txt");
-		Debug.Log("Done");
-		UnityEditor.EditorApplication.isPlaying = false;
+		// lib.Map myMap = new lib.Map();
+		// myMap.generate(100,100, 10);
+		// myMap.printFile(@"Grid.txt");
+		// Debug.Log("Done");
+		// UnityEditor.EditorApplication.isPlaying = false;
 	}
 
 	// Update is called once per frame
