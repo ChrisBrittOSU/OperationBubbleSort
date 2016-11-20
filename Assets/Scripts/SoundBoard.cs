6e29001e7cca2682ae6acd8952734e4282cdff03
Ian Miller <zekemills.phill@gmail.com>
2016-11-20 09:34:28 -0500

    Created soundbard class

---
+++ Assets/Scripts/SoundBoard.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundBoard : MonoBehaviour {
+    public AudioSource bounce, pop, button, victory, walk;
+	
+    public void playBounce()
+    {
+        bounce.Play();
+    }
+
+    public void playPop()
+    {
+        pop.Play();
+    }
+
+    public void playButton()
+    {
+        button.Play();
+    }
+    
+    public void playVictory()
+    {
+        victory.Play();
+    }
+
+    public void playWalk()
+    {
+        walk.Play();
+    }
+}
