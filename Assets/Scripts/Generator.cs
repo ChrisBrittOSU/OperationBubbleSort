2b517aeda7a5fb3a160c88eaabe70ada1a5bafc4
Ian Miller <zekemills.phill@gmail.com>
2016-11-19 22:12:24 -0500

    Created new level for generation testing

---
+++ Assets/Scripts/Generator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Lib;
+using System.Collections;
+
+public class Generator : MonoBehaviour {
+
+    public int x, y;
+    public float difficulty;
+
+    public Map map;
+
+	// Use this for initialization
+	void Start () {
+        map = new Map();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
